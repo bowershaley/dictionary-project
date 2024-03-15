5e375839fa193d49bbfbf6da43ff630daaa3e6d6
bowershaley <156343634+bowershaley@users.noreply.github.com>
2024-03-15 15:46:24 +0000

    added axios and api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data.meanings[0]);
+  }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -11,6 +16,10 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
     alert(`Search for ${keyword}`);
+
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=b0o1e2aa04dbdcb3fatb5f7f5243c844`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   return (
